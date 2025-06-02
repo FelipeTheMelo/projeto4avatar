3b3c2a302c9a3efdff7deded96c2e20b44a377a1
Felipe Melo <melolemoslima@gmail.com>
2025-06-01 22:59:16 -0300

    adicionando public

--- gulpfile.js
+++ gulpfile.js
@@ -4,38 +4,45 @@ const sass = require('gulp-sass')(require('sass'));
 const cleanCSS = require('gulp-clean-css');
 const del = require('del');
 
-// Limpa a pasta dist
+// Limpa a pasta public
 gulp.task('clean', () => {
-    return del(['dist']);
+    return del(['public']);
 });
 
 // Compila SCSS e minifica CSS
 gulp.task('styles', () => {
-    return gulp.src('src/scss/style.scss')  // CAMINHO ATUALIZADO
+    return gulp.src('src/scss/style.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(cleanCSS())
-        .pipe(gulp.dest('dist/css'));
+        .pipe(gulp.dest('public/css'));
 });
 
-// Copia imagens (se houver)
+// Copia imagens
 gulp.task('images', () => {
-    return gulp.src('assets/**/*')
+    return gulp.src('src/images/**/*')
         .pipe(imagemin())
-        .pipe(gulp.dest('dist/assets'));
+        .pipe(gulp.dest('public/images'));
 });
 
 // Copia HTML
 gulp.task('html', () => {
     return gulp.src('src/**/*.html')
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('public'));
+});
+
+// Copia favicon
+gulp.task('favicon', () => {
+    return gulp.src('src/favicon.ico', { allowEmpty: true }) // ADICIONE allowEmpty
+        .pipe(gulp.dest('public'));
 });
 
 // Tarefa principal
-gulp.task('build', gulp.series('clean', gulp.parallel('styles', 'images', 'html')));
+gulp.task('build', gulp.series('clean', gulp.parallel('styles', 'images', 'html', 'favicon')));
 
-// Tarefa de observação automática
+// Tarefa de observação
 gulp.task('watch', () => {
     gulp.watch('src/scss/**/*.scss', gulp.series('styles'));
     gulp.watch('src/**/*.html', gulp.series('html'));
-    gulp.watch('assets/**/*', gulp.series('images'));
+    gulp.watch('src/images/**/*', gulp.series('images'));
+    gulp.watch('src/favicon.ico', gulp.series('favicon'));
 });
